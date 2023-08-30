da0708c28caa39457a2f41dba61349793fb8dce8
Koomaxx86 <141020378+Koomaxx86@users.noreply.github.com>
2023-08-30 09:33:05 +0900

    Add files via upload
    8/30 학원

--- test.sql
+++ test.sql
@@ -7,17 +7,17 @@ CREATE USER joeun_exam IDENTIFIED BY 123456; -- 계정생성
 ALTER USER joeun_exam DEFAULT TABLESPACE users; -- 테이블스페이스 부여
 
 -- 1-3
-ALTER USER joeun QUOTA UNLIMITED ON users; -- 용량설정
+ALTER USER joeun_exam QUOTA UNLIMITED ON users; -- 용량설정
 
 -- 1-4
-GRANT connect, resource to joeun; -- connect, resource 권한 부여
+GRANT connect, resource to joeun_exam; -- connect, resource 권한 부여
 
 -- 1-5
-CREATE TABLE BOARD (
+CREATE TABLE board (
 BOARD_NO        NUMBER              NOT NULL        PRIMARY KEY
-TITLE           VARCHR2(100)        NOT NULL
-CONTENT         VARCHAR2(2000)      NOT NULL
-WRITER          VARCHAR2(20)        NOT NULL
-REG_DATE        DATE                NOT NULL
-UPD_DATE        DATE                NOT NULL
+,TITLE           VARCHAR2(100)        NOT NULL
+,CONTENT         VARCHAR2(2000)      NOT NULL
+,WRITER          VARCHAR2(20)        NOT NULL
+,REG_DATE        DATE                DEFAULT sysdate NOT NULL
+,UPD_DATE        DATE                DEFAULT sysdate NOT NULL
 );
